cb6c280f8b43358720ba80050e36519c7e272905
anastasiya.a.antropova <antropova.anastasiya.a@gmail.com>
2022-10-24 21:38:38 +0200

    add browser test with selene and allure attachments

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -1,10 +1,14 @@
 import allure
 from selene import have, by
+from selene.support.shared import browser
+
+from utils import attach
 
 
 @allure.title("Successful fill form")
-def test_successful(setup_browser):
-    browser = setup_browser
+def test_successful():
+#def test_successful(setup_browser):
+    #browser = setup_browser
     first_name = "Alex"
     last_name = "Egorov"
 
@@ -39,3 +43,7 @@ def test_successful(setup_browser):
         browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
         # browser.element(".table-responsive").should(
         #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
