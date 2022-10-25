161d936dbb377fc5afb823362a0c951d2e1d758c
anastasiya.a.antropova <antropova.anastasiya.a@gmail.com>
2022-10-25 21:55:41 +0200

    add login-password

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-from dotenv import load_dotenv
+from dotenv import load_dotenv #библиотека секретн
 
 from utils import attach
 
@@ -41,12 +41,12 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
-    #login = os.getenv('LOGIN')
-    #password = os.getenv('PASSWORD')
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        #command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        #command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser = Browser(Config(driver))
