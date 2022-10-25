9f0c50c1d09bd7bbae72a50222611047c3c9f718
anastasiya.a.antropova <antropova.anastasiya.a@gmail.com>
2022-10-25 21:35:20 +0200

    add default browser_version

--- conftest.py
+++ conftest.py
@@ -28,12 +28,12 @@ def load_env():
 @pytest.fixture(scope='function')
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
-     #browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion":"100.0",
-        #"browserVersion": browser_version,
+        #"browserVersion":"100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
