fa6bec0c945641dcbd02a30ee0f5fb6065ba73a5
anastasiya.a.antropova <antropova.anastasiya.a@gmail.com>
2022-10-27 19:07:23 +0200

    new project

--- conftest.py
+++ conftest.py
@@ -32,7 +32,7 @@ def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        #"browserVersion":"100.0",
+        #"browserVersion":"100.0", #
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
